f21a647ebc93a2355e74dacb9f760e30b22853ab
SpicyNoodle <522169030@qq.com>
2023-09-23 09:16:11 +0800

    flask query with args

--- app/flask/app.py
+++ app/flask/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
-from arxiv import ArxivAPI, ArxivAsset, ArxivDaily
+from flask import Flask, render_template, request
+from arxiv import ArxivAPI, ArxivAsset, ArxivSet
 from config import CategoryFilterConfig
 from datetime import datetime, timezone, timedelta
+from typing import Callable
 import os
 import json
 
@@ -17,18 +18,63 @@ else:
     config = CategoryFilterConfig({})
 
 
-@app.route("/cs/yesterday")
-def yesterday():
-    global arxiv_asset, config
+def _validate_date(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
 
+
+def _query(date, f: Callable[[ArxivSet], ArxivSet], category=None, primary_set=None):
+    st = arxiv_asset.get_by_date(date, category=category, primary_set=primary_set)
     papers = []
+    if st:
+        st = f(st)
+        papers = st.get_records()
+    return render_template("paperlist.html", date=date, papers=papers)
+
+
+def _query_from_dict(d: dict):
+    pset = d.get("primary_set", None)
+    date = d.get("date", "")
+
+    if pset is None or not _validate_date(date):
+        return render_template("paperlist.html", date="", papers=[])
+
+    def check_and_split(d, attr):
+        if attr in d:
+            if not isinstance(d[attr], list):
+                d[attr] = d[attr].split(",")
+
+    check_and_split(d, "categories")
+    check_and_split(d, "authors")
+    check_and_split(d, "keywd_in_title")
+    check_and_split(d, "keywd_in_abstract")
+
+    cur_config = CategoryFilterConfig(d)
+    return _query(date, f=cur_config.filt, primary_set=pset)
+
+
+@app.route("/query/<pset>")
+def query(pset):
+    d = request.args.to_dict()
+    d["primary_set"] = pset
+    return _query_from_dict(d)
+
+
+@app.route("/cs/")
+def cs_query():
+    d = request.args.to_dict()
+    d["primary_set"] = "cs"
+    return _query_from_dict(d)
+
+
+@app.route("/cs/yesterday")
+def cs_yesterday():
     d_yesterday = datetime.now(timezone.utc) - timedelta(days=1)
     s_yesterday = d_yesterday.strftime("%Y-%m-%d")
-    st = arxiv_asset.get_by_date(s_yesterday, primary_set="cs")
-    if st:
-        st = config.filt(st)
-        papers = st.get_records()
-    return render_template("paperlist.html", date=s_yesterday, papers=papers)
+    return _query(s_yesterday, f=config.filt, primary_set="cs")
 
 
 if __name__ == "__main__":
